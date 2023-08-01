fb2de9594ad6abc470a307db467f9ab20d90b32a
IIIgorMoura <igor2007moura@gmail.com>
2023-08-01 15:45:44 -0300

    a

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 function calcQuad() {
     let a=Number(prompt("Qual é o valor do lado 1 da forma?"));
     let b=Number(prompt("Qual é o valor do lado 2 da forma?"));
-    
-    let result=a*b;
-    alert("A área do retângulo é igual a "+result+"m²");
+
+    let result="A área do retângulo é "+a*b+"m²";
+
+    document.getElementById("valorQuad").innerHTML = result;
 }
 
 function calcTria() {
     let a=Number(prompt("Qual é o tamanho da base da forma?"));
     let b=Number(prompt("Qual é a altura da forma?"));
-    
-    let result=a*b/2;
-    alert("A área do triângulo é "+result+"m²");
+
+    let result="A área do triângulo é "+a*b/2+"m²";
+
+    document.getElementById("valorTria").innerHTML = result;
 }
 
 function calcCirc() {
@@ -19,7 +21,9 @@ function calcCirc() {
 
     let o=Math.PI
     
-    let result=o*a**2;
+    let result="A área do círculo"+o*a**2+"m²";
+    document.getElementById("valorCirc").innerHTML = result;
+
     alert("A área do círculo é a "+result+"m²");
 }
 
@@ -39,4 +43,3 @@ function calcLosa() {
     let result=a*b/2;
     alert("A área da forma é igual a "+result+"m²");
 }
-
